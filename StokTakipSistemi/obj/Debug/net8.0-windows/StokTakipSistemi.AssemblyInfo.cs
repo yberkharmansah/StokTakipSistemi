eb423bbacf0c3ac4f5ef635d1404cdf63bcb568f
yberkharmansah <yberkharmansah@gmail.com>
2025-06-05 01:27:40 +0300

    Merge remote changes into local and resolved conflicts

--- StokTakipSistemi/obj/Debug/net8.0-windows/StokTakipSistemi.AssemblyInfo.cs
+++ StokTakipSistemi/obj/Debug/net8.0-windows/StokTakipSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5468aa68c4031810dbc216a55300e27d0d8fa421")]
 [assembly: System.Reflection.AssemblyProductAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
